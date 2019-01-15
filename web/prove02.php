2b3835fc1dcd56f745f7315f3fb19065f6a93bc5
trosales <thomasrosales27@gmail.com>
2019-01-15 16:00:33 -0700

    assign 2 and style

---
+++ web/prove02.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Home Page</title>
+   <link rel="stylesheet" type="text/css" href="squidward">
+</head>
+<body>
+   <h1>Home page yo!</h1>
+</body>
+</html>

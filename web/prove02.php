ede572b8063c2dbcf4cbd15e5189545256b67483
trosales <thomasrosales27@gmail.com>
2019-01-20 00:19:57 -0700

    the alphabet

--- web/prove02.php
+++ web/prove02.php
@@ -13,7 +13,11 @@
    <?php
       include 'header.php';
    ?>
-   <p>This is the body stuff</p>
+   <p>The alphabet is very diifficult to learn<br>
+   This will help</p>
+   <iframe width="420" height="315"
+   src=https://www.youtube.com/watch?v=g02WKrWjUgA>
+   </iframe>
    <!-- Still needs an IMage and some php stuff -->
 </body>
 </html>

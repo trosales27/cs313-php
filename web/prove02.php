e5a1566bab72a1f7dd710795ecddd56ae2337023
trosales <thomasrosales27@gmail.com>
2019-01-19 23:12:22 -0700

    php stuff??

--- web/prove02.php
+++ web/prove02.php
@@ -6,8 +6,6 @@
 </head>
 <!-- Have a typical navbar on top that floats, then have a big backdrop picture of something musical? -->
 <body>
-   <?php>
-   <?>
    <h1>Home page yo!</h1>
    <div class="header">
       <p>CS313</p>

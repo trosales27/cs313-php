8d8df54c3cba89be7fee6adafedd457cdb54d681
trosales <thomasrosales27@gmail.com>
2019-01-20 00:17:46 -0700

    hacker link

--- web/header.php
+++ web/header.php
@@ -2,5 +2,6 @@
 echo "<div class='options'>
          <a href='prove02.php'>Home Page</a>
          <a href='assignments.php'>Assignments</a>
+         <a style='float:right' href='https://hackertyper.net/'>Wanna be a hacker?></a>
       </div>"
 ?>

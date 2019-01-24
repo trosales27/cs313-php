448206e2fb028fcdd16fd90ff1b8a03d4d01b5e1
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-23 18:57:25 -0700

    style stuff

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="'squidward.css">
+	<link rel="stylesheet" type="text/css" href="squidward.css">
 </head>
 
 <body>
 	<h1>The Music Store (*More creative name coming soon)</h1>
 	<hr>
-	<p>This will be a list of items, with a form to check items to add to cart
+	<p>This will be a list of items, with a form to check items to add to cart</p>
+		<form>
+			<p>This is in the form</p>
+		</form>
 </body>
 </html>

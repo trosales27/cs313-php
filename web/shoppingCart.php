becfcfc4ecd5c6328a96f8f69c23ff608966710e
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-23 22:15:05 -0700

    testing

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -9,7 +9,7 @@
 	<hr>
 	<p>This will be a list of items, with a form to check items to add to cart</p>
 		<form>
-			<div id="row">
+			<div ="row">
 				<a>This is an anchor</a>
 				<p>This is aparagraph</p>
 				<a>another anchor</a>

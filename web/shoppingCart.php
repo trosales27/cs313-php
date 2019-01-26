d6c245b9a16b29d634162d50c053ace97016e710
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-26 13:29:17 -0700

    cleaning

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -13,9 +13,10 @@
 	<p>This will be a list of items, with a form to check items to add to cart</p>
 		<div class="row">
 			<form action="addCart.php" method="post">
-				<p>Stuff here</p>
+				<img src="continuum.jpg">
 				<button type="submit">Add to Cart</button>
 			</form>
+			<br>
 		</div>
 			<div><button onclick="addToCart()">Add to Cart</button></div>
 			<div><button onclick="addToCart()">Add to Cart</button></div>

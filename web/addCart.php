6cffdad24602a6a62207334dbb3d27cade87c58c
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-26 17:31:44 -0700

    adding to cart success

--- web/addCart.php
+++ web/addCart.php
@@ -1,13 +1,19 @@
 <?php
 	session_start();
 
+	print_r($_POST);
+
 	if (sizeof($cart) == 0)
 		$cart = array();
 	else 
 		$cart = $_SESSION['cart'];
-	
-	array_push($cart, $_POST['item']);
-	$_SESSION['cart'] = $cart;
 
+	array_push($cart, $_POST["item"]);
+	$_SESSION['cart'] = $cart;
+	echo "Cart is: ";
+	foreach($cart as $item) {
+		echo $item;
+	}
+	
 	header('Location: shoppingCart.php');
 ?>
--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -12,15 +12,42 @@
 	<hr>
 	<p>This will be a list of items, with a form to check items to add to cart</p>
 		<div class="row">
-			<form action="addCart.php" method="post">
-				<img src="continuum.jpg" alt="continuum keyboard"><br>
-				<button type="submit">Add to Cart</button>
-			</form>
-			<br>
-		</div>
-			<div><button onclick="addToCart()">Add to Cart</button></div>
-			<div><button onclick="addToCart()">Add to Cart</button></div>
-			<div><button onclick="addToCart()">Add to Cart</button></div>
-		</div>
+			<div>
+				<form action="addCart.php" method="post">
+					<img src="continuum.jpg" alt="continuum keyboard"><br>
+					<input type="hidden" value="continuum" name="item">
+					<button type="submit">Add to Cart</button>
+				</form>
+			</div>
+			<div>
+				<form action="addCart.php" method="post">
+					<img src="seaboard.jpeg" alt="roli seaboard keyboard"><br>
+					<input type="hidden" value="seaboard" name="item">
+					<button type="submit">Add to Cart</button>
+				</form>
+			</div>
+			<div>
+				<form action="addCart.php" method="post">
+					<img src="roland-ax-edge.jpg" alt="roland ax-edge"><br>
+					<input type="hidden" value="ax-edge" name="item">
+					<button type="submit">Add to Cart</button>
+				</form>
+			</div>
+			<div>
+				<form action="addCart.php" method="post">
+					<img src="roland-go.jpg" alt="roland go"><br>
+					<input type="hidden" value="roland-go" name="item">
+					<button type="submit">Add to Cart</button>
+				</form>
+			</div>
+			<div>
+				<form action="addCart.php" method="post">
+					<img src="cat-keyboard.jpg" alt="cat keyboard"><br>
+					<input type="hidden" value="cat" name="item">
+					<button type="submit">Add to Cart</button>
+				</form>
+			</div>
+
+
 </body>
 </html>

7a227fadc5e23310e27ca00f83a74b98ce45e49d
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-26 17:38:02 -0700

    cleaning a little

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -16,6 +16,7 @@
 				<form action="addCart.php" method="post">
 					<img src="continuum.jpg" alt="continuum keyboard"><br>
 					<input type="hidden" value="continuum" name="item">
+					<h3>Haken Continuum</h3>
 					<button type="submit">Add to Cart</button>
 				</form>
 			</div>
@@ -23,6 +24,7 @@
 				<form action="addCart.php" method="post">
 					<img src="seaboard.jpeg" alt="roli seaboard keyboard"><br>
 					<input type="hidden" value="seaboard" name="item">
+					<h3>Roli Seaboard</h3>
 					<button type="submit">Add to Cart</button>
 				</form>
 			</div>
@@ -30,6 +32,7 @@
 				<form action="addCart.php" method="post">
 					<img src="roland-ax-edge.jpg" alt="roland ax-edge"><br>
 					<input type="hidden" value="ax-edge" name="item">
+					<h3>Roland Ax-Edge</h3>
 					<button type="submit">Add to Cart</button>
 				</form>
 			</div>
@@ -37,6 +40,7 @@
 				<form action="addCart.php" method="post">
 					<img src="roland-go.jpg" alt="roland go"><br>
 					<input type="hidden" value="roland-go" name="item">
+					<h3>Roland Go</h3>
 					<button type="submit">Add to Cart</button>
 				</form>
 			</div>
@@ -44,6 +48,7 @@
 				<form action="addCart.php" method="post">
 					<img src="cat-keyboard.jpg" alt="cat keyboard"><br>
 					<input type="hidden" value="cat" name="item">
+					<h3>The Kat Keyboard</h3>
 					<button type="submit">Add to Cart</button>
 				</form>
 			</div>

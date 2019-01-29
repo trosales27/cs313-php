4c31eced3e64722efcfaf02d2e1e17292994a134
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-29 15:45:25 -0700

    remove button

--- web/removeCart.php
+++ web/removeCart.php
@@ -8,16 +8,41 @@ session_start();
 	foreach($_SESSION['cart'] as $item) {
 		echo $item . "<br>";
 	}
-	echo "cart size is: " . sizeof($_SESSION['cart']);
-
+    echo "cart size is: " . sizeof($_SESSION['cart']);
+    
+    $cart = array();
 	if (sizeof($_SESSION['cart']) > 0) {
 		foreach ($_SESSION['cart'] as $item) {
-            if ($item == $_POST['item']) {
-                unset($_SESSION[$item]);
-                break;
-            }
+			array_push($cart, $item);
+		}
+	}
+    echo "cart is :\n";
+    foreach($cart as $item) {
+        echo $item;
+    }
+
+	foreach($cart as $item) {
+        if ($item == $_POST['item']) {
+            unset($item);
         }
-    } 
+    }
+    
+    echo "cart is now:\n";
+    foreach($cart as $item) {
+        echo $item;
+    }
+	//array_push($cart, $_POST["item"]);
+	//$_SESSION['cart'] = $cart;
+
+
+	// if (sizeof($_SESSION['cart']) > 0) {
+	// 	foreach ($_SESSION['cart'] as $item) {
+    //         if ($item == $_POST['item']) {
+    //             unset($_SESSION[$item]);
+    //             break;
+    //         }
+    //     }
+    // } 
 
 	echo "Cart is now: ";
 	foreach($_SESSION['cart'] as $item) {

13b7139e8493df28dfb268c6fb6531ec2ea65113
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-29 17:11:17 -0700

    remove button

--- web/removeCart.php
+++ web/removeCart.php
@@ -1,35 +1,46 @@
 <?php
 session_start();
     
-	//echo "Cart is: <br>";
-	foreach($_SESSION['cart'] as $item) {
-		echo $item . "<br>";
-	}
-    //echo "cart size is: " . sizeof($_SESSION['cart']) . "<br>";
-    
-	//array_push($cart, $_POST["item"]);
-	//$_SESSION['cart'] = $cart;
-
-	echo "Cart is: ";
-	foreach($_SESSION['cart'] as $item) {
-		echo $item . "<br>";
-	}
-
+    $cart = array();
 	if (sizeof($_SESSION['cart']) > 0) {
 		foreach ($_SESSION['cart'] as $item) {
-            if ($item == $_POST['item']) {
-                //echo "Match\n";
-                //echo $_SESSION['item'];
-                unset($_SESSION[$item]);
-                break;
-            }
-        }
-    } 
-
-	echo "Cart is now: ";
-	foreach($_SESSION['cart'] as $item) {
+			array_push($cart, $item);
+		}
+    }
+    
+    echo "Cart is: ";
+	foreach($cart as $item) {
 		echo $item . "<br>";
 	}
+
+	// array_push($cart, $_POST["item"]);
+	// $_SESSION['cart'] = $cart;
+
+    if (($key = array_search($_POST['item'], $cart)) !== false) {
+        unset($cart[$key]);
+    }
+
+
+	echo "Cart is: ";
+	foreach($cart as $item) {
+		echo $item . "<br>";
+	}
+
+	// if (sizeof($_SESSION['cart']) > 0) {
+	// 	foreach ($_SESSION['cart'] as $item) {
+    //         if ($item == $_POST['item']) {
+    //             $delItem = $item;
+    //             //unset($_SESSION[$item]);
+    //             break;
+    //         }
+    //     }
+    // } 
+    // unset($_SESSION['cart']);
+
+	// echo "Cart is now: ";
+	// foreach($_SESSION['cart'] as $item) {
+	// 	echo $item . "<br>";
+	// }
 	//echo "cart size is: " . sizeof($_SESSION['cart']) . "<br>";
     
 	//header('Location: cart.php');

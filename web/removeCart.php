54f6c20344dab506130886403704080bf38b8435
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-29 17:16:24 -0700

    finish remove button

--- web/removeCart.php
+++ web/removeCart.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
-    
-echo "Session cart is: ";
-foreach ($_SESSION['cart'] as $item) {
-    echo $item . "<br>";
-}
 
     $cart = array();
 	if (sizeof($_SESSION['cart']) > 0) {
@@ -13,37 +8,11 @@ foreach ($_SESSION['cart'] as $item) {
 		}
     }
 
-	// array_push($cart, $_POST["item"]);
-	// $_SESSION['cart'] = $cart;
-
     if (($key = array_search($_POST['item'], $cart)) !== false) {
         unset($cart[$key]);
     }
 
     $_SESSION['cart'] = $cart;
-
-    echo "Session cart is now: ";
-    foreach ($_SESSION['cart'] as $item) {
-        echo $item . "<br>";
-    }
-
-	// if (sizeof($_SESSION['cart']) > 0) {
-	// 	foreach ($_SESSION['cart'] as $item) {
-    //         if ($item == $_POST['item']) {
-    //             $delItem = $item;
-    //             //unset($_SESSION[$item]);
-    //             break;
-    //         }
-    //     }
-    // } 
-    // unset($_SESSION['cart']);
-
-	// echo "Cart is now: ";
-	// foreach($_SESSION['cart'] as $item) {
-	// 	echo $item . "<br>";
-	// }
-	//echo "cart size is: " . sizeof($_SESSION['cart']) . "<br>";
     
 	header('Location: cart.php');
-
 ?>

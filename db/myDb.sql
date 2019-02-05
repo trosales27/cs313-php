7b6e8cbb7c4841f87d168392f995a75f3c70647b
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-05 10:27:09 -0700

    fixed files

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,1 +1,11 @@
-Databse
+CREATE TABLE song_info(song_id SERIAL PRIMARY KEY,
+	song_name text NOT NULL,
+	album VARCHAR,
+	artist VARCHAR);
+
+CREATE TABLE genre(song_id int REFERENCES song_info(song_id),
+	genre VARCHAR NOT NULL,
+	rating VARCHAR NOT NULL);
+
+CREATE TABLE user_info(user_id SERIAL PRIMARY KEY,
+	username VARCHAR NOT NULL);

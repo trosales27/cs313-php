92f09acb3f4e25e9367396e1477a84ec76a3455b
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-07 16:21:26 -0700

    team activity database

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,11 +1,36 @@
+CREATE TABLE genre(genre_id SERIAL PRIMARY KEY,
+	genre_name VARCHAR(30) NOT NULL);
+
 CREATE TABLE song_info(song_id SERIAL PRIMARY KEY,
 	song_name text NOT NULL,
-	album VARCHAR,
-	artist VARCHAR);
+	album VARCHAR(50),
+	artist VARCHAR(50),
+	rating SMALLINT NOT NULL,
+	genre VARCHAR(30) REFERENCES genre(genre_name));
 
-CREATE TABLE genre(song_id int REFERENCES song_info(song_id),
-	genre VARCHAR NOT NULL,
-	rating VARCHAR NOT NULL);
 
 CREATE TABLE user_info(user_id SERIAL PRIMARY KEY,
-	username VARCHAR NOT NULL);
+	username VARCHAR(50) NOT NULL);
+
+INSERT INTO song_info(song_name, album, artist, rating) VALUES ('The Flood', 'The Congregation', 
+	'Leprous', 5);
+
+SELECT * FROM song_info WHERE rating >= 4;
+
+INSERT INTO genre (genre_name) VALUES ('Rock');
+INSERT INTO genre (genre_name) VALUES ('Alternative');
+INSERT INTO genre (genre_name) VALUES ('Jazz');
+INSERT INTO genre (genre_name) VALUES ('Rap');
+INSERT INTO genre (genre_name) VALUES ('R&B');
+INSERT INTO genre (genre_name) VALUES ('Metal');
+INSERT INTO genre (genre_name) VALUES ('Country');
+INSERT INTO genre (genre_name) VALUES ('Electronic');
+INSERT INTO genre (genre_name) VALUES ('Punk');
+
+--Joining tables together
+SELECT * FROM song_info s
+JOIN genre g ON s.song_id = g.genre_id
+WHERE title = 'The Flood';
+
+-- Updating table info
+-- UPDATE id SET name = 'updtaed info' WHERE id=3;
---
+++ web/team05.php
@@ -0,0 +1,40 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Scripture Resources</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="squidward.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+<h2>Scripture Resources</h2>
+    
+</body>
+</html>

f670ec2df0fa757724a4efec20ee4ab0412de749
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-12 11:11:28 -0700

    team activity

--- web/insert_scriptures.php
+++ web/insert_scriptures.php
@@ -5,8 +5,25 @@ $book = $_POST('book');
 $chapter = $_POST('chapter');
 $verse = $_POST('verse');
 $content = $_POST('content');
-$db->query("INSERT INTO scripture(book, chapter, verse, content) VALUES ($book, $chapter, $verse, $content)");
+
+try {
+
+    $stmt = $db->prepare('INSERT INTO scripture(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
+    $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+    $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+    $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+    $stmt->bindValue(':content', $content, PDO::PARAM_STR);
+    $stmt->execute();
+    //$db->query("INSERT INTO scripture(book, chapter, verse, content) VALUES ($book, $chapter, $verse, $content)");
+}
+catch (PDOException $ex)
+{
+echo 'Error!: ' . $ex->getMessage();
+die();
+}
+
 
 header('Location: display_scriptures.php');
+die();
 
 ?>

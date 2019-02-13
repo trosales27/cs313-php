10578626eec548e0f6a691983560923261d07228
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-12 22:12:52 -0700

    debugging

--- web/insert_scriptures.php
+++ web/insert_scriptures.php
@@ -16,12 +16,12 @@ echo "book is: $book<br> chap is: $chapter<br> verse is: $verse<br> content is: 
  
 // }
 
-    // $stmt = $db->prepare('INSERT INTO scripture(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
-    // $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-    // $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
-    // $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
-    // $stmt->bindValue(':content', $content, PDO::PARAM_STR);
-    // $stmt->execute();
+    $stmt = $db->prepare('INSERT INTO scripture(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
+    $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+    $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+    $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+    $stmt->bindValue(':content', $content, PDO::PARAM_STR);
+    $stmt->execute();
     //$db->query("INSERT INTO scripture(book, chapter, verse, content) VALUES ($book, $chapter, $verse, $content)");
 
 // catch (Exception $ex)

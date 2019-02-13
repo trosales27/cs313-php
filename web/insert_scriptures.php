09868dda40a3e13e209450fee6f28b583a0a51b2
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-12 17:05:16 -0700

    debugging

--- web/insert_scriptures.php
+++ web/insert_scriptures.php
@@ -5,11 +5,8 @@ require("dbConnect.php");
 print_r($_POST);
 echo "book" . $_POST('book');
 $book = htmlspecialchars($_POST('book'));
-echo "<p>Check 1</p>";
 $chapter = htmlspecialchars($_POST('chapter'));
-echo "<p>Check 2</p>";
 $verse = htmlspecialchars($_POST('verse'));
-echo "<p>Check 3</p>";
 $content = htmlspecialchars($_POST('content'));
 foreach($content as $value) {
 
@@ -18,25 +15,25 @@ foreach($content as $value) {
  
  }
 
-// try {
+try {
 
-//     $stmt = $db->prepare('INSERT INTO scripture(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
-//     $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-//     $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
-//     $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
-//     $stmt->bindValue(':content', $content, PDO::PARAM_STR);
-//     $stmt->execute();
-//     //$db->query("INSERT INTO scripture(book, chapter, verse, content) VALUES ($book, $chapter, $verse, $content)");
-// }
-// catch (Exception $ex)
-// {
-// echo 'Error!: ' . $ex->getMessage();
-// echo "<script alert($ex->getMessage())</script>";
-// die();
-// }
+    $stmt = $db->prepare('INSERT INTO scripture(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
+    $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+    $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+    $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+    $stmt->bindValue(':content', $content, PDO::PARAM_STR);
+    $stmt->execute();
+    //$db->query("INSERT INTO scripture(book, chapter, verse, content) VALUES ($book, $chapter, $verse, $content)");
+}
+catch (Exception $ex)
+{
+echo 'Error!: ' . $ex->getMessage();
+echo "<script alert($ex->getMessage())</script>";
+die();
+}
 
 
-//header('Location: display_scriptures.php');
-//die();
+header('Location: display_scriptures.php');
+die();
 
 ?>

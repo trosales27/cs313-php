5213364978af19cc7649041540ead7c793e9c904
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-14 16:08:39 -0700

    folders

--- web/insert_scriptures.php
+++ web/insert_scriptures.php
@@ -10,14 +10,12 @@ if (isset($_POST['new_topic']))
     $new_topic = $_POST['new_topic'];
 }
 
-$stmt = $db->prepare('INSERT INTO scriptures(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
+$stmt = $db->prepare('INSERT INTO scriptures(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)'); /*Insert ia new topic into the topci table here*/
 $stmt->bindValue(':book', $book, PDO::PARAM_STR);
 $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
 $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->execute();
-
-
 
 header('Location: display_scriptures.php');
 die();

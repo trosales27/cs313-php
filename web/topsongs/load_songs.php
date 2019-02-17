009c463150d42bea7c298f54c96714ac1037f69d
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-17 01:16:23 -0700

    adding to database from php

--- web/topsongs/load_songs.php
+++ web/topsongs/load_songs.php
@@ -23,17 +23,17 @@ catch (PDOException $ex)
 
 
 $genres = array();
- foreach ($db->query('SELECT song_name, album, artist, rating FROM song_info WHERE genre = 1') as $song)
+ foreach ($db->query("SELECT song_name, album, artist, rating FROM song_info WHERE genre = 'Rock'") as $song)
     {
       $genres[1] = $song['song_name'];
     }
 
-    foreach ($db->query('SELECT song_name, album, artist, rating FROM song_info WHERE genre = 2') as $song)
+    foreach ($db->query("SELECT song_name, album, artist, rating FROM song_info WHERE genre = 'Metal'") as $song)
     {
       $genres[2] = $song['song_name'];
     }
 
-    foreach ($db->query('SELECT song_name, album, artist, rating FROM song_info WHERE genre = 3') as $song)
+    foreach ($db->query("SELECT song_name, album, artist, rating FROM song_info WHERE genre = 'Jazz'") as $song)
     {
       $genres[3] = $song['song_name'];
     }

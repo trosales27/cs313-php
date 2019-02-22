15cfac19009c9a3609ef0cf8525867801e63abfb
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-22 15:59:43 -0700

    percentage function

--- web/topsongs/vote.php
+++ web/topsongs/vote.php
@@ -12,14 +12,7 @@ $times_voted = $times_voted + 1;
 
 $query="UPDATE song_info SET (times_voted, rating) = ($rating, $times_voted) WHERE song_name = $song AND artist = $artist";
 
-//A number from 1 to 5 will be selected
-//Submit the vote
-//times_voted will increment by 1
-//The selected number will be added to the rating for that song, then divided by the times_voted * 5, returning the new percentage
-
-
-//Will need to revamp database one more time before implementing this.
-
+// need to get this pushed to database
 
 //MAKE THE LIKE THING AN AJAX REQUEST YO
 ?>

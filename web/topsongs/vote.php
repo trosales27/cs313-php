3513a45736b51ca87cd715a196c681b82e8dba32
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-22 16:14:56 -0700

    percentage function

--- web/topsongs/vote.php
+++ web/topsongs/vote.php
@@ -13,7 +13,7 @@ $times_voted = $times_voted + 1;
 $query="UPDATE song_info SET (times_voted, rating) = ($rating, $times_voted) WHERE song_name = $song AND artist = $artist";
 
 $statement = $db->prepare($query);
-$statement>execute();
+$statement->execute();
 
 // need to get this pushed to database
 

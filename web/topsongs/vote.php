934d0ab604acbcb3ed3090e8c61d530f1e5453fe
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-22 16:44:01 -0700

    percentage function

--- web/topsongs/vote.php
+++ web/topsongs/vote.php
@@ -10,10 +10,16 @@ $vote = $_POST['vote'];
 $rating = $rating + $vote;
 $times_voted = $times_voted + 1;
 
-$query="UPDATE song_info SET (times_voted, rating) = ($times_voted, $rating) WHERE song_name = $song AND artist = $artist";
 
-$statement = $db->prepare($query);
-$statement->execute();
+$stmt = $db->prepare('UPDATE song_info(times_voted, rating) SET (:times_voted, :rating)');
+$stmt->bindValue(':times_voted', $tiems_voted, PDO::PARAM_INT);
+$stmt->bindValue(':rating', $rating, PDO::PARAM_INT);
+$stmt->execute();
+
+//$query="UPDATE song_info SET (times_voted, rating) = ($times_voted, $rating) WHERE song_name = $song AND artist = $artist";
+
+//$statement = $db->prepare($query);
+//$statement->execute();
 
 // need to get this pushed to database
 

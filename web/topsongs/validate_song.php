9f8639cae2ba3b837e926a52b28cb94bef0acef4
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-25 23:09:47 -0700

    error checking

--- web/topsongs/validate_song.php
+++ web/topsongs/validate_song.php
@@ -5,12 +5,12 @@ $artist = $_POST['artist'];
 
 $db->query("SELECT song_name FROM song_info WHERE song_name = $song AND artist = $artist");
 
-if ($db->query > 0) {
-    echo "greater than 0";
-}
-else {
-    echo " NOt so";
-}
+// if ($db->query > 0) {
+//     echo "greater than 0";
+// }
+// else {
+//     echo " NOt so";
+// }
 
 //header("Location: submit_song.php");
 ?>

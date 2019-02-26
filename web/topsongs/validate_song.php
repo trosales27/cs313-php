3049854c60464d8607ebe84992d2e06f2a7f086f
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-25 23:11:19 -0700

    error checking

--- web/topsongs/validate_song.php
+++ web/topsongs/validate_song.php
@@ -3,6 +3,8 @@ session_start();
 $song = $_POST["song"];
 $artist = $_POST['artist'];
 
+echo "song is $song and artist is $artist";
+
 $db->query("SELECT song_name FROM song_info WHERE song_name = $song AND artist = $artist");
 
 // if ($db->query > 0) {
